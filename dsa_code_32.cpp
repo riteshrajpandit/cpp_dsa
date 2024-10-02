539091076a16321f076b1273bcad2911ea9d9054
Ritesh Raj Pandit <41156634+riteshrajpandit@users.noreply.github.com>
2024-10-02 13:56:37 +0545

    update to patterns lec 4 dsa

--- dsa_code_32.cpp
+++ dsa_code_32.cpp
@@ -9,23 +9,20 @@ using namespace std;
 
 
 int main() {
-    int n = 4;
+    int n = 8;
     for (int i = 0; i < n; i++) {
-        // Print spaces before the numbers
-        for (int j = 0; j < n - i - 1; j++) {
-            cout << "  ";
+        //spaces
+        for(int j=0;j<n-i-1;j++){
+            cout<<"  ";
         }
-        
-        // Print increasing numbers
-        for (int j = 0; j <= i; j++) {
-            cout << j + 1 << " ";
+        //numbers in increasing order (left side)
+        for(int j=1;j<=i+1;j++){
+            cout<<j<<" ";
         }
-        
-        // Print decreasing numbers
-        for (int j = i; j > 0; j--) {
-            cout << j << " ";
+        //numbers in decreasing order (right side)
+        for(int j=i;j>0;j--){
+            cout<<j<<" ";
         }
-        
         cout << endl;
     }
     return 0;
